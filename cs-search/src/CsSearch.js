656dae845806a0443d7fecd1e6ece6c071c6b49a
Daniel Riedl <daniel.riedl@comsysto.com>
2020-07-03 16:33:22 +0200

    Small updates

--- cs-search/src/CsSearch.js
+++ cs-search/src/CsSearch.js
@@ -1,56 +1,65 @@
 import * as React from "react";
 import PropTypes from "prop-types";
-import {dispatchEvent} from "./service"
-import StyledCsSearch from './CsSearch.styled'
+import { dispatchEvent } from "./service";
+import StyledCsSearch from "./CsSearch.styled";
 
 export class SearchComponent extends React.Component {
-    static propTypes = {
-        name: PropTypes.string
-    };
-    static defaultProps = {
-        name: "Search",
-    };
-    
-    render() {
-        const { name } = this.props;
-        const onClick = () => {
-            dispatchEvent("name", Math.random().toString() + "Test")
-        }
+  static propTypes = {
+    name: PropTypes.string,
+  };
+  static defaultProps = {
+    name: "Search",
+  };
 
-        return (
-            <StyledCsSearch>
-            <div>
-                <input
-                    type="text"
-                    className="cs-search-box"
-                    onkeyup="myFunction()"
-                    placeholder={name}
-                />
-                <ul className="cs-search-list">
-                    <li>
-                        <a href="#" onClick={onClick}>ComSystrom</a>
-                    </li>
-                    <li>
-                        <a href="#" onClick={onClick}>Replystrom</a>
-                    </li>
-                    <li>
-                        <a href="#" onClick={onClick}>Test1</a>
-                    </li>
-                    <li>
-                        <a href="#" onClick={onClick}>Test2</a>
-                    </li>
-                    <li>
-                        <a href="#" onClick={onClick}>Test2</a>
-                    </li>
-                    <li>
-                        <a href="#" onClick={onClick}>Test3</a>
-                    </li>
-                    <li>
-                        <a href="#" onClick={onClick}>Test4</a>
-                    </li>
-                </ul>
-            </div>
-            </StyledCsSearch>
-        );
-    }
+  render() {
+    const { name } = this.props;
+    const onClick = () => {
+      dispatchEvent("name", Math.random().toString() + "Test");
+    };
+
+    return (
+      <StyledCsSearch>
+        <div>
+          <input type="text" placeholder={name} />
+          <ul>
+            <li>
+              <a href="#" onClick={onClick}>
+                ComSystrom
+              </a>
+            </li>
+            <li>
+              <a href="#" onClick={onClick}>
+                Replystrom
+              </a>
+            </li>
+            <li>
+              <a href="#" onClick={onClick}>
+                Test1
+              </a>
+            </li>
+            <li>
+              <a href="#" onClick={onClick}>
+                Test2
+              </a>
+            </li>
+            <li>
+              <a href="#" onClick={onClick}>
+                Test2
+              </a>
+            </li>
+            <li>
+              <a href="#" onClick={onClick}>
+                Test3
+              </a>
+            </li>
+            <li>
+              <a href="#" onClick={onClick}>
+                Test4
+              </a>
+            </li>
+          </ul>
+        </div>
+      </StyledCsSearch>
+    );
+  }
 }
