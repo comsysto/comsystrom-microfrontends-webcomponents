c6e42c49f12358a106ae185926765d27d1b373d8
Norman Steger <n.steger@reply.de>
2020-07-03 14:00:51 +0200

    cs-search replace css with styled-components

--- cs-search/src/CsSearch.js
+++ cs-search/src/CsSearch.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import PropTypes from "prop-types";
+import StyledCsSearch from './CsSearch.styled'
 
 export class SearchComponent extends React.Component {
     static propTypes = {
@@ -12,37 +13,39 @@ export class SearchComponent extends React.Component {
     render() {
         const { name, onHelloEvt } = this.props;
         return (
-            <div>
-                <input
-                    type="text"
-                    className="cs-search-box"
-                    onkeyup="myFunction()"
-                    placeholder={name}
-                />
-                <ul className="cs-search-list">
-                    <li>
-                        <a href="#">ComSystrom</a>
-                    </li>
-                    <li>
-                        <a href="#">Replystrom</a>
-                    </li>
-                    <li>
-                        <a href="#">Test1</a>
-                    </li>
-                    <li>
-                        <a href="#">Test2</a>
-                    </li>
-                    <li>
-                        <a href="#">Test2</a>
-                    </li>
-                    <li>
-                        <a href="#">Test3</a>
-                    </li>
-                    <li>
-                        <a href="#">Test4</a>
-                    </li>
-                </ul>
-            </div>
+            <StyledCsSearch>
+                <div>
+                    <input
+                        type="text"
+                        className="cs-search-box"
+                        onkeyup="myFunction()"
+                        placeholder={name}
+                    />
+                    <ul className="cs-search-list">
+                        <li>
+                            <a href="#">ComSystrom</a>
+                        </li>
+                        <li>
+                            <a href="#">Replystrom</a>
+                        </li>
+                        <li>
+                            <a href="#">Test1</a>
+                        </li>
+                        <li>
+                            <a href="#">Test2</a>
+                        </li>
+                        <li>
+                            <a href="#">Test2</a>
+                        </li>
+                        <li>
+                            <a href="#">Test3</a>
+                        </li>
+                        <li>
+                            <a href="#">Test4</a>
+                        </li>
+                    </ul>
+                </div>
+            </StyledCsSearch>
         );
     }
 }
