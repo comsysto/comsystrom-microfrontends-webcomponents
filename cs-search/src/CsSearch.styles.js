7e8b58100d5f7ea131abe5aac3c3c193fa65ce8e
Norman Steger <me@normansteger.com>
2020-07-11 13:42:13 +0200

    cs-search replace styled-components with material-ui

---
+++ cs-search/src/CsSearch.styles.js
@@ -0,0 +1,14 @@
+export const styles = {
+    search: {},
+    searchField: {
+        margin: '0 0 16px 16px'
+    },
+    searchList: {},
+    searchItem: {
+        borderTop: '1px solid #ddd',
+        '&:last-child': {
+            borderBottom: '1px solid #ddd'
+        },
+        color: '#555'
+    }
+};
